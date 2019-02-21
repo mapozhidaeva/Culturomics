f57989a71d21c2c07efddd9034ea28d9ad658896
annnyway <nuticbooms@gmail.com>
2019-02-21 14:04:33 +0300

    изменен get_entities.py

--- wikidata-entities/get_entities.py
+++ wikidata-entities/get_entities.py
@@ -1,39 +1,44 @@
 import ijson
+LINES = 5000
 
 
-f = open("20150420.json", "r", encoding="UTF-8")
-items = ijson.items(f, "item")
-
-
-def write_tsv(items, lines = 500): #пока возьмем только 500 сущностей из Викиданных
-    n_lines = 0
-
-    for item in items:
-        if item["type"] == "item" and n_lines < lines:
-            n_lines += 1
-            id_ = item["id"]
-            label = ''
-            try:
-                label = item["labels"]["ru"]["value"]
-            except KeyError:
-                pass
-            aliases = ''
-            try:
-                if len(item["aliases"]["ru"]) == 1:
-                    aliases = item["aliases"]["ru"][0]['value']
-                if len(item["aliases"]["ru"]) > 1:
-                    aliases = "|".join([i["value"] for i in item["aliases"]["ru"]])
-            except KeyError:
-                pass
-            
-            with open("entities.tsv", "a", encoding = "UTF-8") as wf:
-                wf.write('%s\t%s\t%s\n' % (id_, label, aliases))
+def write_tsv(items): 
+    with open("entities-5000.tsv", "a", encoding = "UTF-8") as wf:
+        n_lines = 0
+        for item in items:
+            if item["type"] == "item" and n_lines < LINES:    
+                label = ''
+                try:
+                    label = item["labels"]["ru"]["value"]
+                except KeyError:
+                    pass
+                   
+                aliases = ''
+                try:
+                    if len(item["aliases"]["ru"]) == 1:
+                        aliases = item["aliases"]["ru"][0]['value']
+                    if len(item["aliases"]["ru"]) > 1:
+                        aliases = "|".join([i["value"] for i in item["aliases"]["ru"]])
+                except KeyError:
+                    pass 
                 
-        elif item["type"] == "item" and n_lines == lines:
-            break
-           
+                if label != '':
+                    n_lines += 1
+                    id_ = item["id"]
+                    wf.write('{}\t{}\t{}\n'.format(id_, label, aliases))
+                                       
+            elif item["type"] == "item" and n_lines == LINES:
+                break
+                
+    return 0
 
-write_tsv(items)
-f = f.close()
 
-#этот код выполняется примерно 27 секунд
+def main():
+    with open("20150420.json", "r", encoding="UTF-8") as f:
+        items = ijson.items(f, "item")            
+        write_tsv(items)
+    return 0
+  
+                             
+if __name__ == '__main__':
+    main()         
